4952eacfbbd378b4e454932b2c16c2c25afa39c4
yovvel <joffrey.nolting@gmail.com>
2020-02-22 08:16:42 +0100

    ads some feedback to the user about how many files are removed

--- torrentLocationSwitcher.py
+++ torrentLocationSwitcher.py
@@ -10,14 +10,25 @@ torrent_dir_file = os.listdir(torrent_dir)
 downloads_dir ="/home/yovvel/testfolder/downloads/"
 downloads_dir_file= os.listdir(downloads_dir)
 
+added_files = 0
+
 #   step 2: for each *.added file:
 for item in torrent_dir_file:
             #   step 3: remove file
     if item.endswith(".added"):
+        added_files = added_files +  1
         os.remove(os.path.join(torrent_dir, item))
+if added_files > 1:
+    print(str(added_files) + " *.added files removed")
+elif added_files == 1:
+    print("Only 1  *.added found and removed")
+
+else:
+    print("no *.added files removed")
+
 
 #   step 4: go to /home/yovvel/testfolder/downloads/
 for item in downloads_dir_file:
     #   step 5: move each *.torrent to /home/yovvel/testfolder/torrent/
     if item.endswith(".torrent"):
-        shutil.move(downloads_dir+item, torrent_dir)
+        shutil.move(downloads_dir + item, torrent_dir)
