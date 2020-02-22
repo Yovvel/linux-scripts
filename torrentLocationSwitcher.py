78f05cc4b431515146b041f9ca409a8136237918
yovvel <joffrey.nolting@gmail.com>
2020-02-22 07:10:20 +0100

    placed the steps on the right place in the code

--- torrentLocationSwitcher.py
+++ torrentLocationSwitcher.py
@@ -1,19 +1,14 @@
-#   step 1: go to /home/yovvel/testfolder/torrent/
-#   step 2: for each *.added file:
-#   step 3: remove file
-
 #   step 4: go to /home/yovvel/testfolder/downloads/
 #   step 5: move each *.torrent to /home/yovvel/testfolder/torrent/
 
-
-
-
-
 import os
 
+#   step 1: go to /home/yovvel/testfolder/torrent/
 torrent_dir = "/home/yovvel/testfolder/torrent/"
 torrent_dir_file = os.listdir(torrent_dir)
 
+#   step 2: for each *.added file:
 for item in torrent_dir_file:
     if item.endswith(".added"):
+        #   step 3: remove file
         os.remove(os.path.join(torrent_dir, item))
