a3364d11e3b1c73861c35ca5ea667c1ae5a7bbe8
yovvel <joffrey.nolting@gmail.com>
2020-02-22 07:50:49 +0100

    adds the function to move new torrent files to correct folder

--- torrentLocationSwitcher.py
+++ torrentLocationSwitcher.py
@@ -2,13 +2,22 @@
 #   step 5: move each *.torrent to /home/yovvel/testfolder/torrent/
 
 import os
+import shutil
 
 #   step 1: go to /home/yovvel/testfolder/torrent/
 torrent_dir = "/home/yovvel/testfolder/torrent/"
 torrent_dir_file = os.listdir(torrent_dir)
+
+downloads_dir ="/home/yovvel/testfolder/downloads/"
+downloads_dir_file= os.listdir(downloads_dir)
 
 #   step 2: for each *.added file:
 for item in torrent_dir_file:
     if item.endswith(".added"):
         #   step 3: remove file
         os.remove(os.path.join(torrent_dir, item))
+
+
+for item in downloads_dir_file:
+    if item.endswith(".torrent"):
+        shutil.move(downloads_dir+item, torrent_dir)
