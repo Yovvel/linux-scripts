48c0a761badee329d4d1310be217aa9abc06d325
Joffrey Nolting <joffrey.nolting@gmail.com>
2020-02-24 12:41:38 +0100

    added some feedback about torrent files

--- torrentLocationSwitcher.py
+++ torrentLocationSwitcher.py
@@ -11,6 +11,7 @@ downloads_dir ="/home/yovvel/testfolder/downloads/"
 downloads_dir_file= os.listdir(downloads_dir)
 
 added_files = 0
+torrent_files = 0
 
 #   step 2: for each *.added file:
 for item in torrent_dir_file:
@@ -19,18 +20,27 @@ for item in torrent_dir_file:
         added_files = added_files +  1
         os.remove(os.path.join(torrent_dir, item))
 
-#give some feedback to the user about how many files are removed
+#   step 4: go to /home/yovvel/testfolder/downloads/
+for item in downloads_dir_file:
+    #   step 5: move each *.torrent to /home/yovvel/testfolder/torrent/
+    if item.endswith(".torrent"):
+		torrent_files = torrent_files +  1
+        shutil.move(downloads_dir + item, torrent_dir)
+
+
+#give some feedback to the user about how many files are removed and moved
 if added_files > 1:
     print(str(added_files) + " *.added files removed")
 elif added_files == 1:
     print("Only 1  *.added found and removed")
 
-else:
+elif added_files 0= 0:
     print("no *.added files removed")
+	
+if torrent_files > 1:
+    print(str(torrent_files) + " new torrent download files have been added")
+elif torrent_files == 1:
+    print("1 new torrent file added")
 
-
-#   step 4: go to /home/yovvel/testfolder/downloads/
-for item in downloads_dir_file:
-    #   step 5: move each *.torrent to /home/yovvel/testfolder/torrent/
-    if item.endswith(".torrent"):
-        shutil.move(downloads_dir + item, torrent_dir)
+elif torrent_files == 0:
+    print("no Torrent files found")
